2ccafeabe55b584c2d9f979793dfb6dc392ee680
Yash Tupkar <yashtupkar6@gmail.com>
2025-11-05 13:28:41 +0530

    Add components/Footer.jsx

---
+++ components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer className="py-6 text-center text-xs text-slate-400">
+      <p>Serene Mood Tracker &middot; Take a moment for yourself every day</p>
+    </footer>
+  );
+};
+
+export default Footer;
